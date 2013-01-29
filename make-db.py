9afb9708527dc07e9fab6c7b9425e5789c501ebe
nhusted <nhusted@indiana.edu>
2013-01-28 22:50:52 -0500

    Re-factored the make DB program a bit so that it will be easier to add in the DB related code.

--- make-db.py
+++ make-db.py
@@ -3,22 +3,29 @@
 import xml.etree.cElementTree as ET
 
 def main():
+
+    file_list = []
+    # Get a parsed list of etext files. They should only
+    # hae the specific etext number.
+
     tree = ET.parse("catalog.rdf")
     #tree = ET.parse("catalogsample.rdf")
-
     root = tree.getroot()
 
-    #parse_single_book_rdf(root)
-    parse_catalog_rdf(root)
+    # Parse the RDF files keeping in mind the list of etexts
+    # to minimize the information in the database
+    #parse_single_book_rdf(root, file_list)
+    parse_catalog_rdf(root, file_list)
 
 #END
 
 """
 Created to parse the large catalog.rdf files from project gutenberg. They
 contain "etext" and "file" tags for major tags. There are a few others, but
-they did not look of interest (e.g. Description)
+they did not look of interest (e.g. Description). Parsing the RDF
+file also adds the final information to the database if debugging is off. 
 """
-def parse_catalog_rdf(tree_root):
+def parse_catalog_rdf(tree_root, ebook_list):
     for child in tree_root:
         if(child.tag == "{http://www.gutenberg.org/rdfterms/}etext"):
 
@@ -31,23 +38,26 @@ def parse_catalog_rdf(tree_root):
                 print "Not English... skipping..."
                 continue
             #END if
+
+            # Information for DB
+            etextID = ""
+            title = ""
+            author = []
+            subjects = []
+            publisher = ""
             
             # This is the etext ID
-            print child.get("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}ID")
-
-            title = ""
-            title_tag = child.find("{http://purl.org/dc/elements/1.1/}title")
+            etextID = child.get("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}ID")
 
             # Some books are missing the title field. One example had an empty friendly title.
-            if(title_tag is None):
-                title = ""
-            else:
+            title_tag = child.find("{http://purl.org/dc/elements/1.1/}title")
+            if(title_tag is not None):
                 title = title_tag.text.encode("utf-8")
+            #END IF
 
             # Authors can be either single names under creator or multiple names in a list of
             # contributors.
             creator = child.find("{http://purl.org/dc/elements/1.1/}creator")
-            author = [];
             if (creator == None):
                contributors = child.findall("{http://purl.org/dc/elements/1.1/}contributor/{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Bag/{http://www.w3.org/1999/02/22-rdf-syntax-ns#}li")
                for contributor in contributors:
@@ -56,11 +66,9 @@ def parse_catalog_rdf(tree_root):
                 author.append(creator.text.encode("utf-8"))
             #End if
 
-            publisher = child.find("{http://purl.org/dc/elements/1.1/}publisher")
+            publisher_name = child.find("{http://purl.org/dc/elements/1.1/}publisher")
+            publisher =  publisher_name.text.encode("utf-8")
 
-            print publisher.text.encode("utf-8")
-            for name in author:
-                print name
 
             # The full path for the LCSH elements is actually:
             #{http://purl.org/dc/terms/}subject
@@ -68,10 +76,18 @@ def parse_catalog_rdf(tree_root):
             #      {http://www.w3.org/1999/02/22-rdf-syntax-ns#}li
             #          {http://purl.org/dc/terms/}LCSH
             for subject in child.findall(".//{http://purl.org/dc/terms/}LCSH/{http://www.w3.org/1999/02/22-rdf-syntax-ns#}value"):
-                print subject.text.encode("utf-8")
+                subjects.append(subject.text.encode("utf-8"))
             for subject in child.findall(".//{http://purl.org/dc/terms/}LCC/{http://www.w3.org/1999/02/22-rdf-syntax-ns#}value"):
-                print subject.text.encode("utf-8")
+                subjects.append(subject.text.encode("utf-8"))
 
+            # Print out information if we're debugging
+            print etextID
+            print title
+            for name in author:
+                print name
+            print publisher
+            for subject in subjects:
+                print subject
             print "---------------"
         else:
             print("Unparsed tag: %s" % child.tag)
@@ -84,7 +100,7 @@ Parses the single book RDF files. They contain far more information than the
 catalog equivalents. In this case detailed information about authors, file types,
 and the etexts themselves.
 """
-def parse_single_book_rdf(tree_root):
+def parse_single_book_rdf(tree_root, ebook_list):
     #for ebook in root.findall("{http://www.gutenberg.org/2009/pgterms/}ebook"):
     for child in tree_root:
 
