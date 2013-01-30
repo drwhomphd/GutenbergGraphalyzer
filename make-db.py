e32b7e2cf4c9942bb820465c69f7e3f5e69dfc7a
nhusted <nhusted@indiana.edu>
2013-01-30 17:10:13 -0500

    Added scaffolding for the database god. Also made sure catalog information is only printed to the command line in debug mode.

--- make-db.py
+++ make-db.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 
 import xml.etree.cElementTree as ET
+import sqlite3
+import glob
+
+# Given the rare usage of this script the parameters are hardcoded instead
+# of taken from the command line.
+CATALOG_FILE = "catalog.rdf"
+DATABASE_FILE = "catalog.db"
+TEXTS_DIR = "~/nobackup/TEXTS-UNIQUE"
 
 def main():
 
@@ -8,14 +16,14 @@ def main():
     # Get a parsed list of etext files. They should only
     # hae the specific etext number.
 
-    tree = ET.parse("catalog.rdf")
-    #tree = ET.parse("catalogsample.rdf")
+    #tree = ET.parse("catalog.rdf")
+    tree = ET.parse("catalogsample.rdf")
     root = tree.getroot()
 
     # Parse the RDF files keeping in mind the list of etexts
     # to minimize the information in the database
     #parse_single_book_rdf(root, file_list)
-    parse_catalog_rdf(root, file_list)
+    parse_catalog_rdf(root, file_list, debug=True)
 
 #END
 
@@ -25,7 +33,14 @@ contain "etext" and "file" tags for major tags. There are a few others, but
 they did not look of interest (e.g. Description). Parsing the RDF
 file also adds the final information to the database if debugging is off. 
 """
-def parse_catalog_rdf(tree_root, ebook_list):
+def parse_catalog_rdf(tree_root, ebook_list, debug=False):
+
+    # Setup the sqlite connection here so the cursor can be passed
+    # around to the various parse/insertion functions
+    db_conn = sqlite3.connect(DATABASE_FILE)
+    db_curs = db_conn.cursor()
+    
+
     for child in tree_root:
         if(child.tag == "{http://www.gutenberg.org/rdfterms/}etext"):
 
@@ -80,25 +95,37 @@ def parse_catalog_rdf(tree_root, ebook_list):
             for subject in child.findall(".//{http://purl.org/dc/terms/}LCC/{http://www.w3.org/1999/02/22-rdf-syntax-ns#}value"):
                 subjects.append(subject.text.encode("utf-8"))
 
+
+
+            # Add all relevant information to the database
+            add_ebook_to_db(db_curs, ebookID, title, copyright, downloads, filename)
+            add_author_to_db(db_curs, ebookID, author_list)
+            add_subject_to_db(db_curs, ebookID, subject_list)
+
             # Print out information if we're debugging
-            print etextID
-            print title
-            for name in author:
-                print name
-            print publisher
-            for subject in subjects:
-                print subject
-            print "---------------"
+            if(debug == True):
+                print etextID
+                print title
+                for name in author:
+                    print name
+                print publisher
+                for subject in subjects:
+                    print subject
+                print "---------------"
+            #END IF
         else:
             print("Unparsed tag: %s" % child.tag)
         #END IF
     #END FOR
 
+    db_conn.close()
+
+#END FUNCTION
 
 """
 Parses the single book RDF files. They contain far more information than the
-catalog equivalents. In this case detailed information about authors, file types,
-and the etexts themselves.
+catalog equivalents. In this case detailed information about authors, file
+types, and the etexts themselves.
 """
 def parse_single_book_rdf(tree_root, ebook_list):
     #for ebook in root.findall("{http://www.gutenberg.org/2009/pgterms/}ebook"):
@@ -110,7 +137,6 @@ def parse_single_book_rdf(tree_root, ebook_list):
         # We iterate through the children and switch over an ebook or agent
         # because this means we only loop over all entries once instead of
         # with the findall which potentially means we scan the document twice.
-        
         # Only used with single book rdf texts
         if(child.tag == "{http://www.gutenberg.org/2009/pgterms/}ebook"):
             # Get the ebook number
@@ -134,6 +160,24 @@ def parse_single_book_rdf(tree_root, ebook_list):
         else: # Any other tags we don't care about
             continue
 # END
+
+def add_ebook_to_db(cursor, ebookID, title, copyright, downloads, filename):
+    return
+
+"""
+The name field from the catalogue will have the author's birth and death
+date. To safe space both the author is added to author details, if the
+author doesn't currently exist, and then added to bookauthors.
+"""
+def add_author_to_db(cursor, ebookID, author_list):
+    return
+
+"""
+The subject is added to subjectdetails, if it does not already exist,
+and booksubjects at the same time to conserve space. 
+"""
+def add_subject_to_db(cursor, ebookID, subject_list):
+    return
 
 if __name__ == "__main__":
     main()
