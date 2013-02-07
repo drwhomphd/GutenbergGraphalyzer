00cee32fe33a90d30d6f321cb74c564e13ae57e4
nhusted <nhusted@indiana.edu>
2013-02-06 20:14:55 -0500

    Script for executing the full experiment run.

--- run-experiment.sh
+++ run-experiment.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 DB_FILE="catalog.db"
-TEXT_FILES=~/nobackup/TEXTS-UNIQUE/*.txt
+TEXT_FILES=~/nobackup/TEXTS-UNIQUE/
+#TEXT_FILES=./*.txt
 PATH_TO_GRAPHALYZER="./"
+#total=$(ls -l $TEXT_FILES | wc -l)
+THREAD_COUNT=32
 
-for file in $TEXT_FILES
-do
-  $("python $PATH_TO_GRAPHALYZER/graphalyzer.py -i $file -o $DB_FILE")
-done
+counter=0
+
+find $TEXT_FILES/ -name '*.txt' -print | xargs -I CMD --max-procs=$THREAD_COUNT python $PATH_TO_GRAPHALYZER/graphalyzer.py -i CMD -o $DB_FILE
+
+#for file in $TEXT_FILES
+#do
+#  let counter+=1
+#  echo "$counter / $total"
+#  python $PATH_TO_GRAPHALYZER/graphalyzer.py -i $file -o $DB_FILE > OUT.TXT
+#done
