d67e66c1b505be9a35f469846f7699f40c3fd592
Nathaniel Husted <nhusted@gmail.com>
2013-02-24 22:51:59 -0500

    Added properly labeled (but without legend) two plots. One for the etext count per author decade, the other for a information-based complexity change over time.

--- result-analysis.r
+++ result-analysis.r
@@ -15,20 +15,34 @@ dbListTables(con)
 tbl = dbGetQuery(con, "SELECT * FROM experiments")
 print(names(tbl))
 
-avgs = c()
-sds = c()
+avgivd = c()
+avgsi = c()
 
-for(e in seq(1500, 1980, 5)){
-  qry = sprintf("SELECT * FROM experiments exp, authordetails ad, bookauthors ab, ebooks eb WHERE ad.authorID = ab.authorID AND ab.etextID = eb.etextID AND CAST(ad.birth AS INTEGER) >= %d AND CAST(ad.birth AS INTEGER) < %d AND exp.etextID = ab.etextID", e, e+10)
+sdivd = c()
+sdsi = c()
+
+counts = c()
+
+for(e in seq(20, 1971, 10)){
+  qry = sprintf("SELECT DISTINCT(ab.etextID), exp.* FROM experiments exp, authordetails ad, bookauthors ab, ebooks eb WHERE ad.authorID = ab.authorID AND ab.etextID = eb.etextID AND CAST(ad.birth AS INTEGER) >= %d AND CAST(ad.birth AS INTEGER) < %d AND exp.etextID = ab.etextID", e, e+10)
   tbl = dbGetQuery(con, qry)
-  avgs = c(avgs, mean(tbl$da))
-  sds = c(sds, sd(tbl$da))
+  avgivd = c(avgivd, mean(tbl$ivdnorm))
+  sdivd = c(sdivd, sd(tbl$ivdnorm))
+  avgsi = c(avgsi, mean(tbl$sinorm))
+  sdsi = c(sdsi, mean(tbl$sinorm))
+
+  qry = sprintf("SELECT COUNT(DISTINCT(ab.etextID)), exp.* FROM experiments exp, authordetails ad, bookauthors ab, ebooks eb WHERE ad.authorID = ab.authorID AND ab.etextID = eb.etextID AND CAST(ad.birth AS INTEGER) >= %d AND CAST(ad.birth AS INTEGER) < %d AND exp.etextID = ab.etextID", e, e+10)
+  
+  tbl2 = dbGetQuery(con, qry)
+  counts = c(counts, tbl2[,1])
 }
 
-plot(avgs~seq(1500, 1980, 5), type="l")
+plot(counts~seq(20, 1971, 10), type="l", xlab="Author Birth Year", ylab="Ebook Count", main = "Ebook Count Per Author Birth Decade (AD)")
 
+plot(avgsi~seq(20,1971, 10), type="l", xlab="Author Birth Decade(AD)", ylab="Bits", main = "Complexity of Literature")
+lines(seq(20,1971, 10), avgivd, col="red")
 
-tbl = dbGetQuery(con, "SELECT * FROM experiments exp, authordetails ad, bookauthors ab, ebooks eb WHERE ad.authorID = ab.authorID AND ab.etextID = eb.etextID AND CAST(ad.birth AS INTEGER) >= 1820 AND CAST(ad.birth AS INTEGER) < 1830 AND exp.etextID = ab.etextID")
+#tbl = dbGetQuery(con, "SELECT * FROM experiments exp, authordetails ad, bookauthors ab, ebooks eb WHERE ad.authorID = ab.authorID AND ab.etextID = eb.etextID AND CAST(ad.birth AS INTEGER) >= 1820 AND CAST(ad.birth AS INTEGER) < 1830 AND exp.etextID = ab.etextID")
 
 #pairs(tbl)
 
