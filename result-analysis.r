85dec3bf2035dc7291c205f2a542154be99ca32d
Nathaniel Husted <nhusted@gmail.com>
2013-02-28 21:54:56 -0500

    Updated plot lists in R script.

--- result-analysis.r
+++ result-analysis.r
@@ -1,5 +1,6 @@
 library(RSQLite)
 library(ggplot2)
+library(Hmisc)
 
 con <- dbConnect("SQLite", dbname = "results/catalog.db")
 
@@ -26,10 +27,13 @@ counts = c()
 for(e in seq(20, 1971, 10)){
   qry = sprintf("SELECT DISTINCT(ab.etextID), exp.* FROM experiments exp, authordetails ad, bookauthors ab, ebooks eb WHERE ad.authorID = ab.authorID AND ab.etextID = eb.etextID AND CAST(ad.birth AS INTEGER) >= %d AND CAST(ad.birth AS INTEGER) < %d AND exp.etextID = ab.etextID", e, e+10)
   tbl = dbGetQuery(con, qry)
+
   avgivd = c(avgivd, mean(tbl$ivdnorm))
   sdivd = c(sdivd, sd(tbl$ivdnorm))
+  
   avgsi = c(avgsi, mean(tbl$sinorm))
-  sdsi = c(sdsi, mean(tbl$sinorm))
+  sdsi = c(sdsi, sd(tbl$sinorm))
+
 
   qry = sprintf("SELECT COUNT(DISTINCT(ab.etextID)), exp.* FROM experiments exp, authordetails ad, bookauthors ab, ebooks eb WHERE ad.authorID = ab.authorID AND ab.etextID = eb.etextID AND CAST(ad.birth AS INTEGER) >= %d AND CAST(ad.birth AS INTEGER) < %d AND exp.etextID = ab.etextID", e, e+10)
   
@@ -37,10 +41,65 @@ for(e in seq(20, 1971, 10)){
   counts = c(counts, tbl2[,1])
 }
 
+# Plot the book count #########################
+
 plot(counts~seq(20, 1971, 10), type="l", xlab="Author Birth Year", ylab="Ebook Count", main = "Ebook Count Per Author Birth Decade (AD)")
+
+
+# Plot ivd-norm and si-norm complexity #########
 
 plot(avgsi~seq(20,1971, 10), type="l", xlab="Author Birth Decade(AD)", ylab="Bits", main = "Complexity of Literature")
 lines(seq(20,1971, 10), avgivd, col="red")
+legend("topleft", legend = c("sinorm", "ivdnorm"), col = c("black", "red"), lty = c(1, 1))
+
+# Zoom in to 1500-1971  #########################
+
+avgivd = c()
+avgsi = c()
+avgda = c()
+avgaec = c()
+avgnec = c()
+
+sdivd = c()
+sdsi = c()
+sdda = c()
+sdaec = c()
+sdnec = c()
+
+for(e in seq(1500, 1971, 10)){
+  qry = sprintf("SELECT DISTINCT(ab.etextID), exp.* FROM experiments exp, authordetails ad, bookauthors ab, ebooks eb WHERE ad.authorID = ab.authorID AND ab.etextID = eb.etextID AND CAST(ad.birth AS INTEGER) >= %d AND CAST(ad.birth AS INTEGER) < %d AND exp.etextID = ab.etextID", e, e+10)
+  tbl = dbGetQuery(con, qry)
+
+  avgivd = c(avgivd, mean(tbl$ivdnorm))
+  sdivd = c(sdivd, sd(tbl$ivdnorm))
+  
+  avgsi = c(avgsi, mean(tbl$sinorm))
+  sdsi = c(sdsi, sd(tbl$sinorm))
+
+  avgda = c(avgda, mean(tbl$da))
+  sdda = c(sdda, sd(tbl$da))
+
+  avgaec = c(avgaec, mean(tbl$aec))
+  sdaec = c(sdaec, sd(tbl$aec))
+
+  avgnec = c(avgnec, mean(tbl$nec))
+  sdnec = c(sdnec, sd(tbl$nec))
+}
+plot(avgsi~seq(1500,1971, 10), type="l", xlab="Author Birth Decade(AD)", ylab="Bits", main = "Complexity of Literature", ylim=c(0, max(avgsi)))
+lines(seq(1500,1971, 10), avgivd, col="red")
+legend("topright", legend = c("sinorm", "ivdnorm"), col = c("black", "red"), lty = c(1, 1))
+
+#Plot AEC
+plot(avgaec~seq(1500,1971, 10), type="l", xlab="Author Birth Decade(AD)", main = "Complexity of Literature", ylim=c(0,max(avgaec)))
+legend("topright", legend = c("AEC"), col = c("black"), lty = c(1, 1))
+
+#Plot NEC
+plot(avgnec~seq(1500,1971, 10), type="l", xlab="Author Birth Decade(AD)", main = "Complexity of Literature", ylim=c(0,max(avgnec)))
+legend("topright", legend = c("NEC"), col = c("black"), lty = c(1, 1))
+
+#Plot DA
+plot(avgda~seq(1500,1971, 10), type="l", xlab="Author Birth Decade(AD)", main = "Complexity of Literature")
+legend("top", legend = c("DA"), col = c("black"), lty = c(1, 1))
 
 #tbl = dbGetQuery(con, "SELECT * FROM experiments exp, authordetails ad, bookauthors ab, ebooks eb WHERE ad.authorID = ab.authorID AND ab.etextID = eb.etextID AND CAST(ad.birth AS INTEGER) >= 1820 AND CAST(ad.birth AS INTEGER) < 1830 AND exp.etextID = ab.etextID")
 
